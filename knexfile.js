82711a3ac5b341c1304ac275bd5525ace86e501c
pocper1 <pocper1@gmail.com>
2023-04-07 20:21:52 +0800

    update database configuration

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,47 @@
-require('./src/lib/bootstrap');
-const config = require('config');
-const fs = require('fs');
-const path = require('path');
+// Update with your config settings.
 
-const BASE_PATH = path.join(__dirname, 'src', 'database');
-
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
-  local: {
-    client: 'pg',
-    connection: 'postgres://user:112a@localhost:5432/noteflow',
-    migrations: {
-      directory: path.join(BASE_PATH, 'migrations'),
-    },
-    seeds: {
-      directory: path.join(BASE_PATH, 'seeds'),
-    },
-  },
+
   development: {
-    client: 'pg',
-    connection: 'postgres://user:112a@localhost:5432/noteflow',
-    migrations: {
-      directory: path.join(BASE_PATH, 'migrations'),
-    },
-    seeds: {
-      directory: path.join(BASE_PATH, 'seeds'),
-    },
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
   },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
 };
