ecd76c14b9f270ce06aafccca3ff3b2f2c91bd13
pocper1 <pocper1@gmail.com>
2023-04-30 15:11:10 +0800

    disable email verify

--- src/service/userServices/register.js
+++ src/service/userServices/register.js
@@ -44,52 +44,50 @@ const createTransporter = async () => {
 };
 
 const register = async (ctx) => {
-  // const { user } = ctx.request.body; // if none, assign user with {}
+  const { user } = ctx.request.body; // if none, assign user with {}
 
-  // if (!user) {
-  //   ctx.throw(400, "Bad request. You didn't provide user column.");
-  // }
-  // if (!user.email || !user.name || !user.password) {
-  //   ctx.throw(400, "Bad request. You didn't provide sufficient information.");
-  // }
+  if (!user) {
+    ctx.throw(400, "Bad request. You didn't provide user column.");
+  }
+  if (!user.email || !user.name || !user.password) {
+    ctx.throw(400, "Bad request. You didn't provide sufficient information.");
+  }
+
   //email verification
+  // const sendEmail = async (emailOptions) => {
+  //   let emailTransporter = await createTransporter();
+  //   await emailTransporter.sendMail(emailOptions);
+  // };
 
+  // sendEmail({
+  //   subject: "[SDM email verify]",
+  //   text: "email verify",
+  //   to: user.email,
+  //   from: process.env.EMAIL
+  // });
 
-  // send mail with defined transport object
-  const sendEmail = async (emailOptions) => {
-    let emailTransporter = await createTransporter();
-    await emailTransporter.sendMail(emailOptions);
-  };
+  // console.log("Message sent: %s", info.messageId);
+  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-  sendEmail({
-    subject: "Test",
-    text: "I am sending an email from nodemailer!",
-    to: "put_email_of_the_recipient",
-    from: process.env.EMAIL
-  });
+  const result = await db('users').first().where({ email: user.email });
+  if(result) {
+    ctx.throw(401, "Forbidden, you already have  an email.");
+  }
 
-  console.log("Message sent: %s", info.messageId);
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  user.uuid = uuidv4();
+  user.password = await argon2.hash(user.password);
 
-  // const result = await db('users').first().where({ email: user.email });
-  // if(result) {
-  //   ctx.throw(401, "Forbidden, you already have  an email.");
-  // }
+  await db('users').insert(humps.decamelizeKeys(user));
 
-  // user.uuid = uuidv4();
-  // user.password = await argon2.hash(user.password);
+  ctx.session.logined = true;
+  ctx.session.email = user.email;
+  ctx.session.name = user.name;
+  ctx.session.picture = user.picture;
+  await ctx.session.save();
+  await createUserBucket(user.email);
 
-  // await db('users').insert(humps.decamelizeKeys(user));
-
-  // ctx.session.logined = true;
-  // ctx.session.email = user.email;
-  // ctx.session.name = user.name;
-  // ctx.session.picture = user.picture;
-  // await ctx.session.save();
-  // await createUserBucket(user.email);
-
-  // ctx.status = 200;
-  // ctx.body = { user: _.omit(user, ['password']) };
+  ctx.status = 200;
+  ctx.body = { user: _.omit(user, ['password']) };
 };
 
 export default register;
